7d5e5567ba8d67463158eb41edf3c117b9fa57b7
davariijs <Atuor127@gmail.com>
2025-03-24 04:29:04 -0400

    Add JWT authentication and authorization checks to checkout routes

--- src/routes/checkOutCartRoutes.ts
+++ src/routes/checkOutCartRoutes.ts
@@ -1,38 +1,41 @@
 import express, { Request, Response } from 'express';
 import mongoose, { Model } from 'mongoose';
 import { ICheckOut } from '../types';
+import { authenticateJWT } from './authRoutes';
 const router = express.Router();
 const { CheckOutSchema } = require('../models/Schema');
 
 const CheckOutList: Model<ICheckOut>= mongoose.model<ICheckOut>('check-out', CheckOutSchema);
 
-    router.post("/", async (req: Request, res: Response): Promise<void> => {
+    router.post("/", authenticateJWT, async (req: Request, res: Response): Promise<void> => {
         const { uid, orders } = req.body;
 
-        // Validate request body
+        const user = req.body.user;
+        if (user.userId !== uid) {
+            res.status(403).json({ error: "Unauthorized: You can only add  & information to your own CheckOut." });
+            return;
+        }
+
+
         if (!uid || !orders || !Array.isArray(orders)) {
             res.status(400).json({ message: "Invalid request body. 'uid' and 'orders' are required." });
             return;
         }
 
         try {
-            // Find the user's CheckOutList by UID
             let checkOut = await CheckOutList.findOne({ uid });
 
             if (!checkOut) {
-                // If no CheckOutList exists for the user, create a new one
                 checkOut = new CheckOutList({ uid, orders: [] });
             }
 
-            // Add the new orders to the user's checkout list
             checkOut.orders = [...checkOut.orders, ...orders];
 
-            // Save the updated checkout list to the database
             await checkOut.save();
 
             res.status(200).json({
                 message: "Order successfully added to your dashboard",
-                checkOut: checkOut, // Return the entire updated checkout list
+                checkOut: checkOut,
             });
         } catch (error) {
             console.error("Failed to process order:", error);
@@ -42,8 +45,16 @@ const CheckOutList: Model<ICheckOut>= mongoose.model<ICheckOut>('check-out', Che
 
 
 
-    router.get('/:uid', async (req: Request, res: Response): Promise<void> => { 
+    router.get('/:uid', authenticateJWT, async (req: Request, res: Response): Promise<void> => { 
         try {
+            const authenticatedUserId = req.body.user.userId;
+            if (authenticatedUserId !== req.params.uid) {
+                res.status(403).json({ 
+                error: "Unauthorized: You can only access your own Orders." 
+                });
+                return;
+            }
+            
             const checkOutOrder = await CheckOutList.findOne({ uid: req.params.uid });
             res.status(200).json(checkOutOrder);
             } catch (error) {
